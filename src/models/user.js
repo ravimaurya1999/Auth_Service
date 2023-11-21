81f001896345a4da0ff866ccad19183727f1740a
ravimaurya1999 <mauryaravikumar4@gmmail.com>
2023-11-21 09:59:10 +0530

    Signup logic with password encryption done

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+const { SALT } = require('../config/serverConfig');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -11,20 +14,35 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsToMany(models.Role, {
+        through: 'User_Roles',
+      })
     }
   }
   User.init({
-    email:{
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
-      
-  },
-  password: DataTypes.STRING
-},{
-   
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 100]
+      }
+    }
+  }, {
     sequelize,
     modelName: 'User',
+  });
+
+  User.beforeCreate((user) => {
+    const encryptedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = encryptedPassword;
   });
   return User;
 };
